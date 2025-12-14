f8c460f7e91e9d95fac4c490ae7042789c986689
alan <chess@stk.me>
2025-12-15 00:45:04 +0800

    .

--- src/lib.rs
+++ src/lib.rs
@@ -85,13 +85,25 @@ pub fn derive_dynamic_get(input: TokenStream) -> TokenStream {
 /// 或者：
 /// #[dynamic_method(StructName)]
 /// pub fn method_name(&mut self, ...) -> ... { ... }
+// =======================
+// 宏: DynamicGet
+// =======================
+
+/// 动态方法属性宏
+///
+/// 用法：
+/// #[dynamic_method(StructName)]
+/// pub fn method_name(&self, ...) -> ... { ... }
+///
+/// 或者：
+/// #[dynamic_method(StructName)]
+/// pub fn method_name(&mut self, ...) -> ... { ... }
 #[proc_macro_attribute]
 pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
     let struct_name: syn::Ident = parse_macro_input!(args as syn::Ident);
     let method = parse_macro_input!(input as ImplItemFn);
     let method_name = &method.sig.ident;
     let sig = &method.sig;
-
     // 检查是否有接收器（self）
     let has_receiver = sig
         .inputs
@@ -105,11 +117,9 @@ pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
         .to_compile_error()
         .into();
     }
-
     // 确定接收器类型：是否为 &mut self
     let mut needs_mut = false;
     let mut found_self = false;
-
     for arg in &sig.inputs {
         if let FnArg::Receiver(receiver) = arg {
             found_self = true;
@@ -117,7 +127,6 @@ pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
             break; // 只关心第一个参数（应该是self）
         }
     }
-
     if !found_self {
         return syn::Error::new_spanned(
             &method.sig.ident,
@@ -126,7 +135,6 @@ pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
         .to_compile_error()
         .into();
     }
-
     // 生成唯一 const 名
     let const_ident = syn::Ident::new(
         &format!("__DYNAMIC_METHOD_{}_{}", struct_name, method_name)
@@ -134,12 +142,10 @@ pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
             .to_uppercase(),
         method_name.span(),
     );
-
     // 解析参数：跳过 self，只处理显式参数
     let mut arg_downcasts = Vec::new();
     let mut call_args = Vec::new();
     let mut arg_index = 0usize;
-
     for arg in &sig.inputs {
         match arg {
             // 跳过 self 参数
@@ -147,7 +153,6 @@ pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
             FnArg::Typed(pat_type) => {
                 let ty = &*pat_type.ty;
                 let temp_var = syn::Ident::new(&format!("__arg_{}", arg_index), pat_type.span());
-
                 // 只支持简单 ident 或 _
                 if let Pat::Ident(PatIdent { ident, .. }) = &*pat_type.pat {
                     if ident == "_" {
@@ -170,77 +175,69 @@ pub fn dynamic_method(args: TokenStream, input: TokenStream) -> TokenStream {
                     .to_compile_error()
                     .into();
                 }
-
                 arg_index += 1;
             }
         }
     }
-
     // 返回类型
     let _ret_ty = match &sig.output {
         syn::ReturnType::Default => quote!(()),
         syn::ReturnType::Type(_, ty) => quote!(#ty),
     };
-
     // 原始方法定义
     let item = &method;
-
     // 根据方法类型生成不同的代码
     if needs_mut {
         // 可变方法
         let expanded = quote! {
             #item
-
             const #const_ident: () = {
-                use ::alanthinker_dynamic_get_field_trait::MethodInfo;
+                use ::alanthinker_dynamic_get_field_trait::{MethodInfo, MethodKind};
                 use ::inventory;
-
                 inventory::submit! {
-                    MethodInfo::Mutable {
+                    MethodInfo {
+                        type_id: std::any::TypeId::of::<#struct_name>(),
                         name: stringify!(#method_name),
-                        call: move |obj: &mut dyn ::std::any::Any, args: &[&dyn ::std::any::Any]| -> Option<Box<dyn ::std::any::Any>> {
-                            #(#arg_downcasts)*
-
-                            if let Some(this) = obj.downcast_mut::<#struct_name>() {
-                                let result = this.#method_name(#(#call_args),*);
-                                return Some(Box::new(result));
+                        kind: MethodKind::Mutable {
+                            call: move |obj: &mut dyn ::std::any::Any, args: &[&dyn ::std::any::Any]| -> Option<Box<dyn ::std::any::Any>> {
+                                #(#arg_downcasts)*
+                                if let Some(this) = obj.downcast_mut::<#struct_name>() {
+                                    let result = this.#method_name(#(#call_args),*);
+                                    return Some(Box::new(result));
+                                }
+                                None
                             }
-
-                            None
                         }
                     }
                 };
             };
         };
-
         TokenStream::from(expanded)
     } else {
         // 不可变方法
         let expanded = quote! {
             #item
-
             const #const_ident: () = {
-                use ::alanthinker_dynamic_get_field_trait::MethodInfo;
+                use ::alanthinker_dynamic_get_field_trait::{MethodInfo, MethodKind};
                 use ::inventory;
-
                 inventory::submit! {
-                    MethodInfo::Immutable {
+                    MethodInfo {
+                        type_id: std::any::TypeId::of::<#struct_name>(),
                         name: stringify!(#method_name),
-                        call: move |obj: &dyn ::std::any::Any, args: &[&dyn ::std::any::Any]| -> Option<Box<dyn ::std::any::Any>> {
-                            #(#arg_downcasts)*
-
-                            if let Some(this) = obj.downcast_ref::<#struct_name>() {
-                                let result = this.#method_name(#(#call_args),*);
-                                return Some(Box::new(result));
+                        kind: MethodKind::Immutable {
+                            call: move |obj: &dyn ::std::any::Any, args: &[&dyn ::std::any::Any]| -> Option<Box<dyn ::std::any::Any>> {
+                                #(#arg_downcasts)*
+                                if let Some(this) = obj.downcast_ref::<#struct_name>() {
+                                    let result = this.#method_name(#(#call_args),*);
+                                    return Some(Box::new(result));
+                                }
+                                None
                             }
-
-                            None
                         }
                     }
                 };
             };
         };
-
         TokenStream::from(expanded)
     }
 }
